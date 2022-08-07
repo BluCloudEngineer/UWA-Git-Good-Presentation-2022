216a181ad2294a5139dd6b11785aff60192e1a19
BluCloudEngineer <83106283+BluCloudEngineer@users.noreply.github.com>
2022-08-07 22:40:29 +0800

    Refactor/obi wan kenobi logic (#15)
    * Updated logic to use a list
    
    * Added new word for spell checking

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def greeter():
         response = "Wake up Mr Freeman!"
     elif name == "Adam Jensen":
         response = "I never asked for this..."
-    elif name == "Obi-Wan Kenobi" or name == "Obi-Wan" or name == "Obi Wan":
+    elif name in [ "Obi Wan", "Obi-Wan", "Obi-Wan Kenobi" ]:
         response = "Hello There!"
     elif name == "Heavy":
         response = "It costs four hundred thousand dollars to fire this weapon, for twelve seconds."
