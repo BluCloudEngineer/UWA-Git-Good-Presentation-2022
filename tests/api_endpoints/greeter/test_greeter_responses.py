d1fabf7bf398bc73c98e22699d1bae5d43099e6e
Daniel Costantin <thatawscloudengineer@gmail.com>
2022-08-07 21:04:48 +0800

    Updated doc strings and added anonymous user tests

--- tests/api_endpoints/greeter/test_greeter_responses.py
+++ tests/api_endpoints/greeter/test_greeter_responses.py
@@ -3,12 +3,12 @@ from app import app
 
 def test_accept_get_request():
     """
-    Make a GET request to the main webpage, it should return
-    a 200 response and have a Content-Type: header of 
+    Make a HTTP GET request to the main webpage, it should
+    return a 200 response and have a Content-Type header of
     text/html; charset=utf-8
     """
     # Make HTTP response
-    response = app.test_client().get("/")
+    response = app.test_client().get("/greeter")
 
     # Run assertions
     assert response.status_code == 200
@@ -16,14 +16,41 @@ def test_accept_get_request():
 
 def test_reject_post_request():
     """
-    Make a POST request to the main webpage, it should fail
-    and return a 405 error and have a Content-Type: header of
-    text/html; charset=utf-8
+    Make a HTTP POST request to the main webpage, it should
+    fail and return a 405 error and have a Content-Type
+    header of text/html; charset=utf-8
     """
     # Make HTTP response
-    response = app.test_client().post("/")
+    response = app.test_client().post("/greeter")
 
     # Run assertions
     assert response.status_code != 200
     assert response.status_code == 405
     assert response.content_type == "text/html; charset=utf-8"
+
+
+def test_anonymous_user_1():
+    """
+    Make a HTTP GET request to the greeter API endpoint but
+    do not include any parameters
+    """
+    # Make HTTP response
+    response = app.test_client().get("/greeter")
+
+    # Run assertions
+    assert response.status_code == 200
+    assert response.content_type == "text/html; charset=utf-8"
+    assert response.data.decode("utf-8") == "Hi anonymous, what is your name?"
+
+def test_anonymous_user_2():
+    """
+    Make a HTTP GET request to the greeter API endpoint with
+    parameters but do not include "name"
+    """
+    # Make HTTP response
+    response = app.test_client().get("/greeter?food=good&caffeine=low")
+    
+    # Run assertions
+    assert response.status_code == 200
+    assert response.content_type == "text/html; charset=utf-8"
+    assert response.data.decode("utf-8") == "Hi anonymous, what is your name?"
