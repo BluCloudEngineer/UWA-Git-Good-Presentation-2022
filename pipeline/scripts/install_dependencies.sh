07353820dacdadc27e8b6a0f1db1eebf17160d78
Daniel Costantin <thatawscloudengineer@gmail.com>
2022-08-09 02:37:38 +0800

    Updated appspec file and scripts

--- pipeline/scripts/install_dependencies.sh
+++ pipeline/scripts/install_dependencies.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-pwd > pwd.txt
-ls > ls.txt
-#pip3 install -r ../../requirements.txt
+pip3 install -r requirements.txt
+exit 0
--- pipeline/scripts/stop_service.sh
+++ pipeline/scripts/stop_service.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+flaskId=$(ps aux | grep -i python | grep -i flask | awk '{print $2}')
+kill $flaskId
+exit 0
